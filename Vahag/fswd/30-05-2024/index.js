54524c44f420b25b70cad2de39285a5c70560679
Vahag <sargsyanvahag6@gmail.com>
2024-06-01 16:47:58 +0400

    Calculator, second variant

--- Vahag/fswd/30-05-2024/index.js
+++ Vahag/fswd/30-05-2024/index.js
@@ -1,17 +1,25 @@
 const display = document.getElementById("display");
+let justCalculated = true;
 
 function appendToDisplay(input) {
+  if (justCalculated) {
+    display.value = "";
+    justCalculated = false;
+  }
   display.value += input;
 }
 
 function clearDisplay() {
-  display.value = "";
+  display.value = "0";
+  justCalculated = true;
 }
 
 function calculate() {
   try {
     display.value = eval(display.value);
+    justCalculated = true;
   } catch (error) {
     display.value = "Error";
+    justCalculated = true;
   }
 }
